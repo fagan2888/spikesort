6a2d59fd0152ba1bc35f999dce6fc7eaa03dd166
Mat Leonard <leonard.mat@gmail.com>
2015-03-11 14:25:24 -0700

    Added the pub_format function from the memory analysis package into the plots module.

--- plots.py
+++ plots.py
@@ -309,4 +309,41 @@ def correlogram(t1, t2=None, bin_width=.001, limit=.02, auto=False):
         count = count[::-1]
 
     return count, bins
+
+def pub_format(ax, legend=None, spine_width=1.3, spine_color='#111111', 
+               hide_spines=['top', 'right'], 
+               yticks=3, xticks=3, yminorticks=1, xminorticks=1):
+    """ Format plots for publication. """
+    if hide_spines is None:
+        pass
+    else:
+        for each in hide_spines:
+            ax.spines[each].set_color('w')
+    ax.set_axis_bgcolor('w')
+    
+    shown_spines = [each for each in ['top', 'bottom', 'left', 'right']
+                    if each not in hide_spines]
+    for each in shown_spines:
+        ax.spines[each].set_linewidth(spine_width)
+        ax.spines[each].set_color(spine_color)
+    
+    ax.yaxis.set_major_locator(MaxNLocator(yticks))
+    ax.xaxis.set_major_locator(MaxNLocator(xticks))
+    ax.yaxis.set_minor_locator(MaxNLocator(yticks*yminorticks))
+    ax.xaxis.set_minor_locator(MaxNLocator(xticks*xminorticks))
+    
+    ax.tick_params(axis='both', which='minor', length=3, width=1.3, 
+                   direction='out', colors='#111111')
+    ax.tick_params(axis='both', which='major', length=4, width=1.3, 
+                   direction='out', colors='#111111')
+    ax.tick_params(which='both', **dict(zip(hide_spines, ['off']*len(hide_spines))))
+    ax.tick_params(direction='out', width=spine_width, color=spine_color)
+    ax.grid(which='both', b=False)
+
+    if legend:
+        leg_frame = legend.get_frame()
+        leg_frame.set_facecolor('w')
+        leg_frame.set_edgecolor('none')
+    
+    return ax
     
